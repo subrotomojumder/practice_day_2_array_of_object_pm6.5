f3094d4532b600e6bc87328e7686220227cc2e0c
Janardhan-Majumder <janardhan.md03@gmail.com>
2026-01-20 19:50:24 +0600

    git add  .

---
+++ word_count.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s, x;
+    getline(cin, s);
+    cin >> x;
+    int count_john = 0;
+    stringstream ss(s);
+
+    string wordn;
+    cin >> wordn;
+    cout << wordn;
+    
+    // cout << count_john;
+    return 0;
+}
